feb9329b1beca4270093735013dc26425a17e33f
tmfrldu <tmfrldu@naver.com>
2023-07-27 20:53:54 +0900

    all commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <hgroup class="my-5">
+      <h1>나의 할일</h1>
+      <em>{{ today }}</em>
+    </hgroup>
+  </header>
+  <ToDoListContainer />
+</template>
+
+<script lang="ts">
+export default {
+  name: "App",
+}
+
+</script>
+
+<script setup lang="ts">
+import {  inject } from 'vue';
+import ToDoListContainer from './components/ToDoListContainer.vue';
+
+const today = inject('today')
+
+</script>
+<style scoped>
+hgroup {
+  text-align: center;
+  font-family: 'Arial Bold';
+}
+
+hgroup h1 {
+  font-weight: bolder;
+}
+
+
+
+</style>
